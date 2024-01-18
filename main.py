bb3691908e8a4f882b61d020092f117b3dbac41c
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-01-18 11:59:33 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+﻿import os
+print("РУ")
+
+def analyze_folder(folder_path):
+    try:
+        # Получаем список файлов и подпапок
+        contents = os.listdir(folder_path)
+
+        print(f"Содержимое папки {folder_path}:")
+        for item in contents:
+            item_path = os.path.join(folder_path, item)
+
+            # Проверяем, является ли элемент файлом или подпапкой
+            if os.path.isfile(item_path):
+                print(f"Файл: {item}")
+            elif os.path.isdir(item_path):
+                print(f"Папка: {item}")
+
+    except FileNotFoundError:
+        print(f"Папка {folder_path} не найдена.")
+    except PermissionError:
+        print(f"Отсутствует доступ к папке {folder_path}.")
+
+# Замените 'путь/к/папке' на путь к вашей папке
+folder_path = 'C:\Users\Asus\Desktop\analyze image\images'
+analyze_folder(folder_path)
