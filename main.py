cac08fb40053d08615d75df8058778a0f0434631
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-01-20 19:04:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-﻿import os
-print("РУ")
+folder_path = input("Enter your adress for folder without "": ")
 
-def analyze_folder(folder_path):
-    try:
-        # Получаем список файлов и подпапок
-        contents = os.listdir(folder_path)
 
-        print(f"Содержимое папки {folder_path}:")
-        for item in contents:
-            item_path = os.path.join(folder_path, item)
+if os.path.exists(folder_path):
 
-            # Проверяем, является ли элемент файлом или подпапкой
-            if os.path.isfile(item_path):
-                print(f"Файл: {item}")
-            elif os.path.isdir(item_path):
-                print(f"Папка: {item}")
+    files = os.listdir(folder_path)
 
-    except FileNotFoundError:
-        print(f"Папка {folder_path} не найдена.")
-    except PermissionError:
-        print(f"Отсутствует доступ к папке {folder_path}.")
 
-# Замените 'путь/к/папке' на путь к вашей папке
-folder_path = 'C:\Users\Asus\Desktop\analyze image\images'
-analyze_folder(folder_path)
+    print("files in folder :")
+    
+
+    file_list = []
+    for file in files:
+        print(file)
+        file_list.append(file)
+
+        images_list = [file for file in file_list if file.endswith('.png')]
+
+
+    print("PNG files in :", images_list)
+
+else:
+    print("Указанной папки не существует.")
