9812350959ca88a466ccb57917f9940c0fe5ed1b
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-01-21 19:50:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 import easyocr
 
 
-folder_path = input("Enter your adress for folder without "": ")
+folder_path = input("Enter your adress for folder without "" with your pictures: ")
+
+
 
 
 if os.path.exists(folder_path):
@@ -28,8 +30,14 @@ if os.path.exists(folder_path):
 
     print("Full paths of PNG files in ready_list:", ready_list)
 
-else:
-    print("Указанной папки не существует.")
+    amount_files = len(ready_list)
+    print("Number of files:", amount_files)
+
+    for i in range(1, amount_files + 1):
+        globals()[f"text_{i}"] = f"This is text for file {i}"
+
+else: print('folder dont exist')
+
 
 
 def text_recognition(file_path):
@@ -38,10 +46,28 @@ def text_recognition(file_path):
 
     return result
 
-def main():
-    file_path = ready_list[0]
-    print(text_recognition(file_path=file_path))
 
+
+
+def main():
+
+    folder_for_save = input("Enter your adress for folder to text your pictures "": ")
+
+    if os.path.exists(folder_for_save):
+
+
+        text_variables = []
+
+        for i, file_path in enumerate(ready_list):
+            text_variable_name = f"text_{i + 1}"
+            text_variables.append(text_variable_name)
+            locals()[text_variable_name] = text_recognition(file_path=file_path)
+            print(f'{text_variable_name}: {locals()[text_variable_name]}')
+
+
+            text_1 = os.path.join(folder_for_save, "text_1.txt")
+
+         
 if __name__ == "__main__":
     main()
 
