95bacfee6f7071116956e9ee407a4aed92e2cc94
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-01-21 13:34:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import os
+import easyocr
+
+
 folder_path = input("Enter your adress for folder without "": ")
 
 
 if os.path.exists(folder_path):
 
     files = os.listdir(folder_path)
-
-
     print("files in folder :")
     
 
@@ -16,8 +18,32 @@ if os.path.exists(folder_path):
 
         images_list = [file for file in file_list if file.endswith('.png')]
 
-
     print("PNG files in :", images_list)
+
+    ready_list = []
+
+    for image in images_list:
+        full_path = os.path.join(folder_path, image)
+        ready_list.append(full_path)
+
+    print("Full paths of PNG files in ready_list:", ready_list)
 
 else:
     print("Указанной папки не существует.")
+
+
+def text_recognition(file_path):
+    reader = easyocr.Reader(["en"])
+    result = reader.readtext(file_path, detail=0, paragraph=True)
+
+    return result
+
+def main():
+    file_path = ready_list[0]
+    print(text_recognition(file_path=file_path))
+
+if __name__ == "__main__":
+    main()
+
+
+
